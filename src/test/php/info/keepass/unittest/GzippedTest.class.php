cb45849ec6d8b0ae5f4902ea699b66b41a7278d7
Timm Friebe <thekid@thekid.de>
2016-07-13 10:41:17 +0200

    Extend test suite to cover decompression

--- src/test/php/info/keepass/unittest/GzippedTest.class.php
+++ src/test/php/info/keepass/unittest/GzippedTest.class.php
@@ -4,10 +4,11 @@ use info\keepass\Gzipped;
 use lang\FormatException;
 
 class GzippedTest extends \unittest\TestCase {
+  const COMPRESSED = "\037\213\010\000\000\000\000\000\000\013\013I-.\001\0002\321Mx\004\000\000\000";
 
   #[@test]
   public function gzencoded_test() {
-    new Gzipped("\037\213\b\000\000\000\000\000\000\v\vI-.\001\0002\321Mx\004\000\000\000");
+    new Gzipped(self::COMPRESSED);
   }
 
   #[@test, @expect(class= FormatException::class, withMessage= '/Too short/')]
@@ -18,5 +19,17 @@ class GzippedTest extends \unittest\TestCase {
   #[@test, @expect(class= FormatException::class, withMessage= '/Magic bytes mismatch/')]
   public function invalid_data() {
     new Gzipped('******************');
+  }
+
+  #[@test]
+  public function method() {
+    $gzipped= new Gzipped(self::COMPRESSED);
+    $this->assertEquals(Gzipped::INFLATE, $gzipped->header()['method']);
+  }
+
+  #[@test]
+  public function decompress() {
+    $gzipped= new Gzipped(self::COMPRESSED);
+    $this->assertEquals('Test', $gzipped->decompress());
   }
 }
