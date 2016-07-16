5224d4d62113004999e78bec2c738ff83b7e7a86
Timm Friebe <thekid@thekid.de>
2016-07-16 23:15:32 +0200

    Test entries, groups and passwords iteration

--- src/test/php/info/keepass/unittest/GroupTest.class.php
+++ src/test/php/info/keepass/unittest/GroupTest.class.php
@@ -1,13 +1,43 @@
 <?php namespace info\keepass\unittest;
 
 use info\keepass\Group;
+use info\keepass\Entry;
+use info\keepass\ProtectedValue;
 use util\UUID;
 
 class GroupTest extends \unittest\TestCase {
-  private $group;
+  private $entry, $child, $group;
 
   /** @return void */
   public function setUp() {
+    $this->entry= [
+      'UUID'            => 'fZhlFzAGRU24qsKpoxQ2Lg==',
+      'IconID'          => '42',
+      'ForegroundColor' => null,
+      'BackgroundColor' => null,
+      'OverrideURL'     => null,
+      'Tags'            => null,
+      'Times'           => '',
+      'String'          => [
+        'Notes'    => null,
+        'Password' => new ProtectedValue("\323\$c", "\274J\006"),
+        'Title'    => 'Entry #1',
+        'URL'      => null,
+        'UserName' => null
+      ]
+    ];
+    $this->child= [
+      'UUID'                    => 'G73VLfHwkU+do8hF9gmofQ==',
+      'Name'                    => 'Child',
+      'Notes'                   => null,
+      'IconID'                  => '48',
+      'Times'                   => '',
+      'IsExpanded'              => 'False',
+      'DefaultAutoTypeSequence' => null,
+      'EnableAutoType'          => 'null',
+      'EnableSearching'         => 'null',
+      'LastTopVisibleEntry'     => 'AAAAAAAAAAAAAAAAAAAAAA==',
+    ];
     $this->group= [
       'UUID'                    => 'Vy28plCgw0u5Y0PtP2c/2Q==',
       'Name'                    => 'Test',
@@ -19,6 +49,12 @@ class GroupTest extends \unittest\TestCase {
       'EnableAutoType'          => 'null',
       'EnableSearching'         => 'null',
       'LastTopVisibleEntry'     => 'AAAAAAAAAAAAAAAAAAAAAA==',
+      'Entry'                   => [
+        'fZhlFzAGRU24qsKpoxQ2Lg==' => $this->entry
+      ],
+      'Group'                   => [
+        'G73VLfHwkU+do8hF9gmofQ==' => $this->child
+      ]
     ];
   }
     
@@ -50,6 +86,30 @@ class GroupTest extends \unittest\TestCase {
   #[@test]
   public function icon_field() {
     $this->assertEquals('48', (new Group($this->group, '/Test'))->field('IconID'));
+  }
+
+  #[@test]
+  public function entries() {
+    $this->assertEquals(
+      ['7d986517-3006-454d-b8aa-c2a9a314362e' => new Entry($this->entry, '/Test/Entry #1')],
+      iterator_to_array((new Group($this->group, '/Test'))->entries())
+    );
+  }
+
+  #[@test]
+  public function groups() {
+    $this->assertEquals(
+      ['1bbdd52d-f1f0-914f-9da3-c845f609a87d' => new Group($this->child, '/Test/Child')],
+      iterator_to_array((new Group($this->group, '/Test'))->groups())
+    );
+  }
+
+  #[@test]
+  public function passwords() {
+    $this->assertEquals(
+      ['/Test/Entry #1' => new ProtectedValue("\323\$c", "\274J\006")],
+      iterator_to_array((new Group($this->group, '/Test'))->passwords())
+    );
   }
 }
 
